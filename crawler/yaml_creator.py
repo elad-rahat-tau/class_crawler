2001af96213407f147ef4b276c4b3c9584e468ba
odyssey <eladrahat@mail.tau.ac.il>
2024-01-17 18:37:58 +0200

    yaml creator

--- crawler/yaml_creator.py
+++ crawler/yaml_creator.py
@@ -6,10 +6,11 @@ import yaml
 
 def get_yaml(file_server, depth=100):
 
-    links_yaml = open("C:/users/user/desktop/links.yaml","w")
+    links_yaml = open("/path for saving the yaml","w")
 
-    url_crawls = crawl.Crawl(file_server, depth, find_urls=find_urls.FindUrls())
+    url_crawls = crawl.Crawl(file_server, depth=depth, find_urls=find_urls.FindUrls())
     url_crawls_list = url_crawls.web_of_links()
 
     for i in range(len(url_crawls_list)):
         yaml.dump({"level": i, "links": url_crawls_list[i]}, links_yaml)
+
